74dfdaeb4c50b459459418186fbd2b09bcaf7bc1
Paul Grosen <pcgrosen@gmail.com>
2019-01-11 09:42:55 -0800

    add admin-login challenge

---
+++ challenges/admin-login/init.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# If you're just trying to solve the challenge, you can safely ignore this file.
+
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+
+cd "$DIR"
+CHALLENGE=$(basename "$DIR")
+
+apt install virtualenv
+
+virtualenv "${CHALLENGE}-env"
+source "${CHALLENGE}-env/bin/activate"
+
+pip install flask
+
+JUMBLE=$(python -c 'import random; import string; print("".join(random.choice(string.ascii_letters) for _ in range(64)))')
+echo "595g{$JUMBLE}" > flag
+
+cat > /lib/systemd/system/595g-${CHALLENGE}.service <<EOF
+[Unit]
+Description=systemd service for 595g challenge $CHALLENGE
+
+[Service]
+User=$CHALLENGE
+Group=$CHALLENGE
+Environment=PATH=$DIR/${CHALLENGE}-env/bin
+WorkingDirectory=$DIR
+ExecStart=$DIR/${CHALLENGE}-env/bin/python server.py
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable 595g-${CHALLENGE}
+systemctl start 595g-${CHALLENGE}
---
+++ challenges/admin-login/server.py
@@ -0,0 +1,48 @@
+import os
+import random
+import string
+import sqlite3
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+admin = "admin"
+here = os.path.dirname(os.path.abspath(__file__))
+flag_path = os.path.join(here, "flag")
+db_path = os.path.join(here, "users.db")
+db = sqlite3.connect(db_path)
+
+c = db.cursor()
+c.execute("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT)")
+db.commit()
+
+c.execute("SELECT * FROM users WHERE username=?", (admin,))
+if not c.fetchone():
+    password = "".join(random.choice(string.ascii_letters) for _ in xrange(64))
+    c.execute("INSERT INTO users VALUES (?, ?)", (admin, password))
+    db.commit()
+c.close()
+db.close()
+
+def get_flag():
+    with open(flag_path, "rb") as f:
+        return f.read().strip()
+
+@app.route("/", methods=["GET", "POST"])
+def login_page():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        cursor = sqlite3.connect(db_path).cursor()
+        cursor.execute("SELECT * FROM users WHERE username='%s' AND password='%s'" %
+                       (request.form["username"], request.form["password"]))
+        if cursor.fetchone():
+            if request.form["username"] == admin:
+                return render_template("flag.html", flag=get_flag())
+            else:
+                return "Well, you logged in, but you aren't an admin."
+        else:
+            return "Hey! You're not an admin!", 403
+
+if __name__ == "__main__":
+    app.run(port=5001)
